ead75910944ddc3843e521ffcac911206f2d67ee
FennellPamela-FS <pyfennell@student.fullsail.edu>
2022-12-05 22:25:30 -0500

    Update db and routes

--- db/db.js
+++ db/db.js
@@ -1,7 +1,18 @@
-const findUser = async () => { };
+const findUser = async (obj) => {
+    // one line of code pass in the obj ?!? email?
+    // await User.findOne(obj).exec() 
+
+    // let user = await User.findOne({
+    //     where: {
+    //         id: obj.id
+    //     }
+    // })
+};
 
 
-const saveUser = async () => { };
+const saveUser = async (user) => {
+    user.saveUser;
+};
 
-
+// connect and disconnect runs for the test only
 module.exports = { connect, findUser, saveUser, disconnect };
